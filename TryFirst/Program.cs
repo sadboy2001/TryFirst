04cfa4ba47f6f34e62544fba3664ccf04d9742a4
gaira <gaira@DESKTOP-6KOLHNU>
2023-01-11 22:38:36 +0300

    Добавьте файлы проекта.

---
+++ TryFirst/Program.cs
@@ -0,0 +1,37 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+
+app.UseAuthorization();
+
+//app.MapRazorPages();
+
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+    //endpoints.MapControllerRoute("_Layout","{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
